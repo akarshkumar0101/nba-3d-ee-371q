0c607fc0c0f1f8fd938f6f7be41155840b37be11
kdesai2018 <kushkdesai@gmail.com>
2020-11-05 21:38:26 -0600

    change from mac

--- ball_detection.py
+++ ball_detection.py
@@ -2,4 +2,10 @@ import detectron2
 
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from 
+from detectron2.data import MetadataCatalog
+import cv2
+import requests
+import numpy as np
+
+cfg = get_cfg()
+cfgFile = '/home/dirac/miniconda3/envs/detectron/lib/python3.6/site-packages/detectron2/'
