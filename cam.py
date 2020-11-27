ac766f89fe7de0f9e82a0d00ec7313f646ddb1dc
Akarsh Kumar <akarshkumar0101@gmail.com>
2020-11-26 23:13:09 -0600

    make scripts np/torch invariant

--- cam.py
+++ cam.py
@@ -5,29 +5,6 @@ import cv2
 
 import util, constants, draw, cam
 
-"""
-This method takes in 2 DoFs that mean "log fx" and "log fy".
-This is useful for having direct representation.
-
-dofs_f has shape (..., 2)
-returns fxy of shape (..., 2)
-"""
-def calc_fxy_direct(dofs_f):
-    return np.e**dofs_f[..., :]
-
-"""
-This method takes in 2 DoFs that mean "log fx" and "log (fy/fx)".
-This is useful for keeping the ratio of fy/fx consistent.
-
-dofs_f has shape (..., 2)
-returns fxy of shape (..., 2)
-"""
-def calc_fxy_ratio(dofs_f):
-    fx = np.e**dofs_f[..., [0]]
-    fy = fx*np.e**dofs_f[..., [1]]
-    return torch.cat([fx, fy], dim=-1)
-    
-
 def calc_dofs_cam(cam_position, cam_points_to, dofs_f):
     bs = cam_position.shape[:-1]
 
@@ -45,34 +22,64 @@ def calc_dofs_cam(cam_position, cam_points_to, dofs_f):
     axang = util.so3_log_map(R)
     return torch.cat((cam_position, axang, dofs_f), dim=-1)
 
-# returns shape (cam, point, 2)
-def project_to_cam(X_w, dofs_cam, calc_fxy=calc_fxy_ratio):
-    bs_X_w, bs_dofs_cam = X_w.shape[:-1], dofs_cam.shape[:-1]
-    for _ in range(len(bs_X_w)):
-        dofs_cam = dofs_cam[..., None, :]
-    for _ in range(len(bs_dofs_cam)):
-        X_w = X_w[None, ...]
-    
+def get_mat_view(dofs_cam):
     R = util.so3_exponential_map(dofs_cam[..., 3:6]) # this goes from camera->world
     Rinv = R.transpose(-1, -2) # world->camera
     T = dofs_cam[..., :3]
+    if type(dofs_cam) is torch.Tensor:
+        mat_view = torch.eye(4).repeat(*dofs_cam.shape[:-1],1,1)
+    else:
+        mat_view = np.tile(np.eye(4, dtype=np.float32), (*dofs_cam.shape[:-1],1,1))
+    mat_view[..., :3, :3] = Rinv[..., :3, :3]
+    mat_view[..., :3, 3] = (-Rinv @ (T[..., :, None]))[..., 0]
+    return mat_view
+
+def calc_perspective_image(img, dofs_cam, mat_model, img_shape_xy):
+    if type(dofs_cam) is torch.Tensor:
+        dofs_cam = dofs_cam.numpy()
+    img_shape_yx = img_shape_xy[::-1]
+    mat_view = get_mat_view(dofs_cam)
+    f = np.e**dofs_cam[..., 6]
+    mat_int = cam.apply_focus(f, cam.get_intrinsic_mat_for_img_shape(img_shape_xy))
     
-    X_c = (Rinv @ ((X_w-T)[..., None]))[..., 0]
+    H = mat_int@mat_view@mat_model[:, [0, 1, 3]]
     
-    dofs_f = dofs_cam[..., 6:]
-    fxy = calc_fxy(dofs_f)
+    bs = dofs_cam.shape[:-1]
+    results = np.zeros(bs + img_shape_yx+img.shape[2:], dtype=img.dtype)
+    results = results.reshape((-1,)+img_shape_yx+img.shape[2:])
+    for i, Hi in enumerate(H.reshape(-1, 3, 3)):
+        results[i, ...] = cv2.warpPerspective(img, Hi, img_shape_xy)
+    results = results.reshape(bs+img_shape_yx+img.shape[2:])
+    return results
+
+# returns shape (cam, point, 2)
+def project_to_cam(X_w, dofs_cam, img_shape_xy):
+    mat_view = get_mat_view(dofs_cam)
+    f = np.e**dofs_cam[..., 6]
+    mat_int = cam.apply_focus(f, cam.get_intrinsic_mat_for_img_shape(img_shape_xy))
     
-    X_i = fxy * ((X_c/X_c[..., [-1]])[..., :2])
+    bs_dofs = dofs_cam.shape[:-1]
+    bs_p = X_w.shape[:-1]
+    for _ in range(len(bs_p)):
+        mat_int = mat_int[..., None, :, :]
+        mat_view = mat_view[..., None, :, :]
     
-    vis_mask = torch.logical_and(X_i>=-1., X_i<=1).all(dim=-1)
-    vis_mask = torch.logical_and(vis_mask, X_c[..., 2]>0.)
-    return X_i, vis_mask
+    X_i = (mat_int @ mat_view @ (util.to_homo(X_w)[..., :, None]))[..., 0]
+    z_buf = X_i[..., 2]
+    X_i = util.from_homo(X_i)
+    
+    xmin, xmax, ymin, ymax = 0, img_shape_xy[0], 0, img_shape_xy[1]
+    viz_mask_x = np.logical_and(X_i[..., 0]>=xmin, X_i[..., 0]<=xmax)
+    viz_mask_y = np.logical_and(X_i[..., 1]>=ymin, X_i[..., 1]<=ymax)
+    viz_mask = np.logical_and(viz_mask_x, viz_mask_y)
+    viz_mask = np.logical_and(viz_mask, z_buf>0)
+    return X_i, viz_mask
 
 
 # returns a crop such that:
 # x is cropped (xmin, xmax) left to right
 # y is cropped (ymin, ymax) down to up
-def get_intrinsic_mat(xmin, xmax, ymin, ymax):
+def get_mat_intrinsic(xmin=-1., xmax=1., ymin=-1., ymax=1.):
     u_0 = (xmin+xmax)/2.
     v_0 = (ymin+ymax)/2.
 
@@ -87,20 +94,24 @@ def get_intrinsic_mat(xmin, xmax, ymin, ymax):
     m_y = (ymax-ymin)/(ylim--ylim)
     
     # -m_x is needed to make +X axis point right and not left in camera/projection coordinates
-    P = np.array([[ -m_x,     0, u_0, 0],
-                  [    0,   m_y, v_0, 0],
-                  [    0,     0,   1, 0]])
-    return P
+    uf_mat_int = np.array([[ -m_x,     0, u_0, 0],
+                           [    0,   m_y, v_0, 0],
+                           [    0,     0,   1, 0]], dtype=np.float32)
+    return uf_mat_int
 
-def apply_focus(f, P):
-    f = np.array(f)
-    P = np.tile(P, (*f.shape, 1, 1))
-    P[..., [0, 1], [0, 1]] = f[..., None]*P[..., [0, 1], [0, 1]]
-    return P
+def apply_focus(f, uf_mat_int):
+    if type(f) is torch.Tensor:
+        mat_int = torch.from_numpy(uf_mat_int).repeat(*f.shape, 1, 1)
+        mat_int[..., [0, 1], [0, 1]] = f*mat_int[..., [0, 1], [0, 1]]
+    else:
+        f = np.array(f)
+        mat_int = np.tile(uf_mat_int, (*f.shape, 1, 1))
+        mat_int[..., [0, 1], [0, 1]] = f[..., None]*mat_int[..., [0, 1], [0, 1]]
+    return mat_int
 
-def get_intrinsic_mat_default():
-    return get_intrinsic_mat(-1., 1., -1., 1.)
+def get_intrinsic_mat_for_img_shape(img_shape_xy):
+    return get_mat_intrinsic(0, img_shape_xy[0], img_shape_xy[1], 0)
 
-def get_intrinsic_mat_for_img_shape(img_shape):
-    return get_intrinsic_mat(0, img_shape[1], img_shape[0], 0)
+def get_intrinsic_mat_for_dofs_cam(dofs_cam, img_shape_xy):
+    return apply_focus(np.e**dofs_cam[..., 6], get_intrinsic_mat_for_img_shape(img_shape_xy))
 
--- util.py
+++ util.py
@@ -6,7 +6,7 @@ import cv2
 import util, constants, draw, cam
 
 # THIS IS ADITYA'S CODE
-def so3_log_map_(R, eps=1e-5):
+def so3_log_map_impl(R, eps=1e-5):
     R33 = R[:3, :3]
     # direction: unit eigenvector of R33 corresponding to eigenvalue of 1
     w, W = torch.eig(R33.t(), eigenvectors=True)
@@ -26,20 +26,30 @@ def so3_log_map_(R, eps=1e-5):
     angle = torch.atan2(sina, cosa)
     return direction* angle
 def so3_log_map(R, eps=1e-5):
+    is_numpy = type(R) is np.ndarray
+    if is_numpy:
+        R = torch.from_numpy(R)
     bs = R.shape[:-2]
     bs_tot = np.prod(bs, dtype=int)
     R = R.view(bs_tot, 3, 3)
     axangs = torch.zeros(bs_tot, 3)
     for i in range(bs_tot):
-        axangs[i] = so3_log_map_(R[i])
-    return axangs.reshape(*bs, 3)
+        axangs[i] = so3_log_map_impl(R[i], eps)
+    axangs = axangs.reshape(*bs, 3)
+    if is_numpy:
+        axangs = axangs.numpy()
+    return axangs
 
 
-def so3_exponential_map(axang, homogeneous = False):
+def so3_exponential_map(axang, homogeneous=False):
     """
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
     """
+    is_numpy = type(axang) is np.ndarray
+    if is_numpy:
+        axang = torch.from_numpy(axang)
+    
     #axis = np.asarray(axis)
     theta = axang.norm(dim=-1)
     axis = axang/theta[..., None]
@@ -60,17 +70,41 @@ def so3_exponential_map(axang, homogeneous = False):
                         torch.stack([2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)], dim=-1),
                         torch.stack([2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc], dim=-1)],
                        dim=-2)
+    if homogeneous:
+        extra_dims = axis.shape[:-1]
+        zeros_column = torch.zeros_like(mat)[..., :, 0:1] # shape: (..., 3, 1)
+        hstacked = torch.cat([mat, zeros_column], dim=-1) # shape: (..., 3, 4)
 
-    if not homogeneous:
-        return mat
+        hom_row = torch.cat((torch.zeros_like(hstacked)[..., :1, :3],
+                             torch.ones_like(hstacked)[..., :1, 3:]), dim=-1) # shape: (..., 1, 4)
+        mat = torch.cat((hstacked, hom_row), dim=-2)
+        
+    if is_numpy:
+        mat = mat.numpy()
+    return mat
+        
 
-    extra_dims = axis.shape[:-1]
-    zeros_column = torch.zeros_like(mat)[..., :, 0:1] # shape: (..., 3, 1)
-    hstacked = torch.cat([mat, zeros_column], dim=-1) # shape: (..., 3, 4)
+def to_homo(X, dim=-1):
+    if type(X) is torch.Tensor:
+        X = X.transpose(dim, -1)
+        X = torch.cat((X, torch.ones(X.shape[:-1]+(1,))), dim=-1)
+        X = X.transpose(dim, -1)
+    else:
+        X = np.swapaxes(X, dim, -1)
+        X = np.concatenate([X, np.ones(X.shape[:-1]+(1,))], axis=-1).astype(X.dtype)
+        X = np.swapaxes(X, dim, -1)
+    return X
 
-    hom_row = torch.cat((torch.zeros_like(hstacked)[..., :1, :3],
-                         torch.ones_like(hstacked)[..., :1, 3:]), dim=-1) # shape: (..., 1, 4)
-    return torch.cat((hstacked, hom_row), dim=-2)
+def from_homo(X, dim=-1):
+    if type(X) is torch.Tensor:
+        X = X.transpose(dim, -1)
+        X = X[..., :-1]/X[..., -1, None]
+        X = X.transpose(dim, -1)
+    else:
+        X = np.swapaxes(X, dim, -1)
+        X = (X[..., :-1]/X[..., -1, None]).astype(X.dtype)
+        X = np.swapaxes(X, dim, -1)
+    return X
 
 # def translation_matrix(tvec: torch.Tensor) -> torch.Tensor:
 #     extra_dims = tvec.shape[:-1]
@@ -88,48 +122,3 @@ def so3_exponential_map(axang, homogeneous = False):
 #     trans_mat = translation_matrix(T)
 #     return trans_mat @ rot_mat
 
-
-def to_homo(X, axis=-1):
-    X = np.swapaxes(X, axis, -1)
-    X = np.concatenate([X, np.ones(X.shape[:-1]+(1,))], axis=-1)
-    X = np.swapaxes(X, axis, -1)
-    return X
-
-def from_homo(X, axis=-1):
-    X = np.swapaxes(X, axis, -1)
-    X = X[..., :-1]/X[..., -1, None]
-    X = np.swapaxes(X, axis, -1)
-    return X
-    
-# def to_homo(X, dim=-1):
-#     X = X.transpose(dim, -1)
-#     X = torch.cat((X, torch.ones(X.shape[:-1]+(1,))), dim=-1)
-#     X = X.transpose(dim, -1)
-#     return X
-# def from_homo(X, dim=-1):
-#     X = X.transpose(dim, -1)
-#     X = X[..., :-1]/X[..., -1, None]
-#     X = X.transpose(dim, -1)
-#     return X
-
-
-# def project_to_cam(X_w, dofs_cam=rest_dofs, f=f, m=(m_x, m_y), uv=(u_0, v_0), ret_vis_mask=False):
-    
-#     X_w = to_homo(X_w)
-#     X_c = (dofs2mat(dofs_cam) @ (X_w[..., None]))[..., 0]
-#     vis_mask = from_homo(X_c)[..., 2]>0.
-#     mat_in = torch.tensor([[m[0], 0., uv[0], 0.],
-#                            [0., m[1], uv[1], 0.],
-#                            [0., 0., 1., 0.]])
-#     mat_in *= (torch.tensor([[0., 1., 1., 1.],
-#                              [1., 0., 1., 1.],
-#                              [1., 1., 1., 1.]])\
-#               +f*torch.tensor([[1., 0., 0., 0.],
-#                                [0., 1., 0., 0.],
-#                                [0., 0., 0., 0.]]))
-    
-#     X_i = (mat_in@(X_c[..., None]))[..., 0]
-#     X_i = from_homo(X_i)
-#     if ret_vis_mask:
-#         return X_i, vis_mask
-#     return X_i
