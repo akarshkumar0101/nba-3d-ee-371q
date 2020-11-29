cbe0573764a86d927ab3d9b616cd46382ff0bc1d
Akarsh Kumar <akarshkumar0101@gmail.com>
2020-11-29 04:27:38 -0600

    add functions for erase point cloud

--- img_proc.py
+++ img_proc.py
@@ -16,8 +16,10 @@ def process_img(img):
 
 def process_img_canny(img, ep=True):
     img = to_uint8(img)
-    img = cv2.edgePreservingFilter(img)
-    canny = cv2.Canny(img, 50, 150, apertureSize=3)
+    if ep:
+        img = cv2.edgePreservingFilter(img)
+#     canny = cv2.Canny(img, 50, 150, apertureSize=3)
+    canny = cv2.Canny(img, 25, 50, apertureSize=3)
     return canny
 
 def erase_players(img, kp, width=25):
@@ -27,3 +29,24 @@ def erase_players(img, kp, width=25):
     blank = cv2.dilate(blank, np.ones((width, width), dtype=np.uint8))
     img[blank>100] = 0
     return img
+def erase_players_points(img_shape_xy, points, kp, width=25):
+    blank = np.zeros(img_shape_xy[::-1], dtype=np.uint8)
+    blank = draw.draw_people_img(blank, kp, color=255, thickness=1)
+    blank = cv2.dilate(blank, np.ones((width, width), dtype=np.uint8))
+    mask = blank>100
+    points = points.astype(int)
+    pointmask = ~mask[points[:, 1], points[:, 0]]
+    return pointmask
+
+
+def erase_scorebox(img, score_box):
+    img = img.copy()
+    ((x0,y0), (x1,y1)) = score_box
+    img[y0:y1, x0: x1] = 0
+    return img
+
+def erase_scorebox_points(points, score_box):
+    x, y = points[:, 0], points[:, 1]
+    ((x0,y0), (x1,y1)) = score_box
+    return ~np.logical_and(np.logical_and(x>x0, x<x1), np.logical_and(y>y0, y<y1))
+
